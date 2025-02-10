4d2c47a32d84c9cb727c2b99497a516d7afa85ac
Christopher Schmidt <christopher.schmidt@kosickaakademia.sk>
2025-02-10 10:41:06 +0100

    added json property annotation

--- src/main/java/com/example/employee_task_management/model/AssignmentStatus.java
+++ src/main/java/com/example/employee_task_management/model/AssignmentStatus.java
@@ -1,7 +1,14 @@
 package com.example.employee_task_management.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum AssignmentStatus {
+    @JsonProperty("PENDING")
     PENDING,
+
+    @JsonProperty("IN_PROGRESS")
     IN_PROGRESS,
+
+    @JsonProperty("COMPLETED")
     COMPLETED
 }
