29884e622d4d98bb6490a93491d3875b01f72716
Christopher Schmidt <christopher.schmidt@kosickaakademia.sk>
2025-02-10 08:34:18 +0100

    add Task entity with fields and relationships to Assignment

---
+++ src/main/java/model/Task.java
@@ -0,0 +1,32 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private String deadline;
+
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Assignment> assignments = new HashSet<>();
+}
