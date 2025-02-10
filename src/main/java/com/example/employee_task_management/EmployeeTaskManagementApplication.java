7589e77c6f8ec9caa9bb3efba5638eb8d37c2fdb
Christopher Schmidt <christopher.schmidt@kosickaakademia.sk>
2025-02-10 10:07:02 +0100

    configure component scanning for EmployeeTaskManagementApplication

--- src/main/java/com/example/employee_task_management/EmployeeTaskManagementApplication.java
+++ src/main/java/com/example/employee_task_management/EmployeeTaskManagementApplication.java
@@ -3,7 +3,7 @@ package com.example.employee_task_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.employee_task_management")
 public class EmployeeTaskManagementApplication {
 
     public static void main(String[] args) {
